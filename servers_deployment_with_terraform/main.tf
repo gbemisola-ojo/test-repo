03a9a3a942195229e3c61487911bf0cf2147e4ea
Akinwunmi Akinrimisi <akinolaakinrimisi@gmail.com>
2022-07-19 13:49:23 +0100

    servers deployement with terraform update

--- servers_deployment_with_terraform/main.tf
+++ servers_deployment_with_terraform/main.tf
@@ -7,56 +7,56 @@ data "aws_ssm_parameter" "instance_ami" {
 }
 
 # Creating the VPC
-resource "aws_vpc" "jenkins_vpc" {
+resource "aws_vpc" "servers_vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "jenkins_vpc"
+    Name = "servers_vpc"
   }
 }
 
 # Creating the Internet Gateway
-resource "aws_internet_gateway" "project_igw" {
-  vpc_id = aws_vpc.jenkins_vpc.id
+resource "aws_internet_gateway" "servers_igw" {
+  vpc_id = aws_vpc.servers_vpc.id
   tags = {
-    Name = "project_igw"
+    Name = "servers_igw"
   }
 }
 
 # Creating the public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.jenkins_vpc.id
-  cidr_block              = var.public_subnet_cidr_block
+resource "aws_subnet" "servers_public_subnet" {
+  vpc_id                  = aws_vpc.servers_vpc.id
+  cidr_block              = var.servers_public_subnet_cidr_block
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
   tags = {
-    Name = "public_subnet"
+    Name = "servers_public_subnet"
   }
 }
 
 # Creating the public route table
-resource "aws_route_table" "project_public_rt" {
-  vpc_id = aws_vpc.jenkins_vpc.id
+resource "aws_route_table" "servers_public_rt" {
+  vpc_id = aws_vpc.servers_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.project_igw.id
+    gateway_id = aws_internet_gateway.servers_igw.id
   }
 }
 
 
 # Associating our public subnet with our public route table
-resource "aws_route_table_association" "public" {
-  route_table_id = aws_route_table.project_public_rt.id
-  subnet_id      = aws_subnet.public_subnet.id
+resource "aws_route_table_association" "servers_public" {
+  route_table_id = aws_route_table.servers_public_rt.id
+  subnet_id      = aws_subnet.servers_public_subnet.id
 }
 
 
 # Creating a security group for the Jenkins server
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins_sg"
+resource "aws_security_group" "servers_sg" {
+  name        = "servers_sg"
   description = "Security group for jenkins server"
-  vpc_id      = aws_vpc.jenkins_vpc.id
+  vpc_id      = aws_vpc.servers_vpc.id
 
   ingress {
     description = "allow anyone on port 22"
@@ -98,28 +98,102 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   tags = {
-    Name = "jenkins_sg"
+    Name = "servers_sg"
   }
 }
 
-# Creating an EC2 instance called jenkins_server
-resource "aws_instance" "jenkins_server" {
+# Creating an EC2 instance called cba_jenkins_server
+resource "aws_instance" "cba_jenkins_server" {
   ami                    = data.aws_ssm_parameter.instance_ami.value
-  subnet_id              = aws_subnet.public_subnet.id
+  subnet_id              = aws_subnet.servers_public_subnet.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  key_name               = var.aws_key_pair
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[0]
   user_data              = fileexists("install_jenkins.sh") ? file("install_jenkins.sh") : null
   tags = {
-    Name = "jenkins_server"
+    Name = "cba_jenkins_server"
+  }
+}
+
+# Creating an EC2 instance called cba_student_jenkins_server
+resource "aws_instance" "cba_student_jenkins_server" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  subnet_id              = aws_subnet.servers_public_subnet.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[0]
+  user_data              = fileexists("install_jenkins.sh") ? file("install_jenkins.sh") : null
+  tags = {
+    Name = "cba_student_jenkins_server"
+  }
+}
+
+# Creating an EC2 instance called cba_ansible_controller
+resource "aws_instance" "cba_ansible_controller" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  subnet_id              = aws_subnet.servers_public_subnet.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[1]
+  user_data              = fileexists("install_ansible.sh") ? file("install_ansible.sh") : null
+  tags = {
+    Name = "cba_ansible_controller"
+  }
+}
+
+# Creating an EC2 instance called cba_ansible_slave
+resource "aws_instance" "cba_ansible_slave" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  subnet_id              = aws_subnet.servers_public_subnet.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[1]
+  #user_data              = fileexists("install_ansible.sh") ? file("install_ansible.sh") : null
+  tags = {
+    Name = "cba_ansible_slave"
+  }
+}
+
+# Creating an EC2 instance called cba_K8s_cluster
+resource "aws_instance" "cba_K8s_cluster_01" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  subnet_id              = aws_subnet.servers_public_subnet.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[2]
+  user_data              = fileexists("install_k8s.sh") ? file("install_k8s.sh") : null
+  tags = {
+    Name = "cba_k8s_cluster_01"
+  }
+}
+
+# Creating an EC2 instance called cba_K8s_cluster
+resource "aws_instance" "cba_K8s_cluster" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  subnet_id              = aws_subnet.servers_public_subnet.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  key_name               = var.aws_key_pair[2]
+  user_data              = fileexists("install_k8s.sh") ? file("install_k8s.sh") : null
+  tags = {
+    Name = "cba_k8s_cluster"
   }
 }
 
 # Creating an Elastic IP called jenkins_eip
-resource "aws_eip" "jenkins_eip" {
-  instance = aws_instance.jenkins_server.id
+resource "aws_eip" "cba_jenkins_eip" {
+  instance = aws_instance.cba_jenkins_server.id
   vpc      = true
   tags = {
     Name = "jenkins_eip"
+  }
+}
+
+# Creating an Elastic IP called jenkins_eip
+resource "aws_eip" "cba_student_jenkins_server_eip" {
+  instance = aws_instance.cba_student_jenkins_server.id
+  vpc      = true
+  tags = {
+    Name = "cba_student_jenkins_server_eip"
   }
 }
--- servers_deployment_with_terraform/variables.tf
+++ servers_deployment_with_terraform/variables.tf
@@ -1,22 +1,22 @@
 variable "aws_region" {
-  default = "us-west-2"
+  default = "eu-west-2"
 }
 
 variable "availability_zone" {
   type    = string
-  default = "us-west-2a"
+  default = "eu-west-2b"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.1.0.0/16"
 }
 
-variable "public_subnet_cidr_block" {
+variable "servers_public_subnet_cidr_block" {
   description = "CIDR block for public subnet"
   type        = string
-  default     = "10.0.3.0/24"
+  default     = "10.1.1.0/24"
 }
 
 variable "instance_type" {
@@ -26,7 +26,7 @@ variable "instance_type" {
 }
 
 variable "aws_key_pair" {
-  description = "keypair for Jenkins"
-  type        = string
-  default     = "jenkins_keyname"
+  description = "keypair for Servers; Jenkins, Ansible & K8s"
+  type        = list(string)
+  default     = ["Jenkins-keypair", "Ansible_Server_keypair", "K8s_Server_keypair"]
 }
